43a010261f1f161a9f992a935553face8bfa1ae2
Man Hei Ho <man.ho@mail.utoronto.ca>
2024-01-03 16:07:42 -0500

    updated organizations serializers and views

--- venturebuild/permissions.py
+++ venturebuild/permissions.py
@@ -15,3 +15,21 @@ class IsStaffUser(permissions.IsAdminUser):
     def has_permission(self, request, view):
         if request.user.is_staff: return True
         return False
+    
+# Allows only owner
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.user.owner
+            and request.user.organization == obj
+        )
+    
+# Owner or readonly
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return (
+            request.user.owner
+            and request.user.organization == obj
+        )
